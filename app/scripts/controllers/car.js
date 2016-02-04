5d2187e8b981e34a78de8a933460e8c396f54661
Stephen Smith <stephen304@gmail.com>
2016-02-03 20:07:59 -0500

    feat: Redirect to search if somebody tried to jump to a car ID.

--- app/scripts/controllers/car.js
+++ app/scripts/controllers/car.js
@@ -8,8 +8,14 @@
  * Controller of the hotwireSearchApp
  */
 angular.module('hotwireSearchApp')
-  .controller('CarCtrl', function ($scope, $stateParams, searchService) {
+  .controller('CarCtrl', function ($scope, $state, $stateParams, searchService) {
     $scope.car = searchService.getCar($stateParams.id);
+
+    // If somebody jumped straight to this URL, send them to search
+    // Future addition could make an API call to fetch the car they referenced
+    if ($scope.car === null) {
+      $state.go('search');
+    }
 
     this.awesomeThings = [
       'HTML5 Boilerplate',
